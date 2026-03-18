f1b07a8e10cbd0b055e8b0f77fec9efd1916baa5
BenLeland <blelan@uw.edu>
2026-03-18 14:35:08 -0700

    Improve basic navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,10 +5,15 @@ import './Navbar.css';
 export default function Navbar() {
     return (
         <nav class="container">
-            <p><strong>BEN LELAND</strong></p>
-            <Link to='/'> Home</Link>
-            <Link to='/portfolio'> Portfolio</Link>
-            <Link to='/contact'> Contact</Link>
+            <div>
+                <p class="navName"><strong>BEN LELAND</strong></p>
+            </div>
+            
+            <div class="links">
+                <Link to='/'> Home</Link>
+                <Link to='/portfolio'> Portfolio</Link>
+                <Link to='/contact'> Contact</Link>
+            </div>
         </nav>
     )
 }
