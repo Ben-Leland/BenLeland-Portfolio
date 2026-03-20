52c3bfd903982209e019b183eb640205fe572586
Ben Leland <ben_leland@comcast.net>
2026-03-19 18:45:19 -0700

    Update navbar with inline elements, add hamburger for mobile navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    
     return (
-        <nav class="container">
-            <div>
-                <p class="navName"><strong>BEN LELAND</strong></p>
+        <nav className='container'>
+            <div className='brand'>
+                <p className='symbol'>&lt;&gt;</p>
+                <p className='navName'><strong>BEN LELAND</strong></p>
             </div>
-            
-            <div class="links">
-                <Link to='/'> Home</Link>
-                <Link to='/portfolio'> Portfolio</Link>
-                <Link to='/contact'> Contact</Link>
+
+            <button className='hamburger' onClick={() => setIsOpen(!isOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+
+            <div className={`mobile-menu ${isOpen ? 'open' : ''}`}>
+                <Link to='/' onClick={() => setIsOpen(false)}>Home</Link>
+                <Link to='/portfolio' onClick={() => setIsOpen(false)}>Portfolio</Link>
+                <Link to='/contact' onClick={() => setIsOpen(false)}>Contact Me</Link>
             </div>
+
+            <div className={`overlay ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(false)}></div>
         </nav>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,26 +2,35 @@ import React from 'react';
 import './Home.css';
 import Section from '../../components/Section/Section.jsx';
 import aboutText from './Sections/about.js';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
         <div>
-            <div class="container">
+            <div className="container home">
                 <div>
-                    <img src='/headshot.jpg' alt="Headshot" class="headshot"></img>
-                </div>
-                <div>
-                    <strong><p class="name">
+                    <img src='/headshot.jpg' alt="Headshot" className="headshot"></img>
+
+                    <p className="name">
                         BEN LELAND
                     </p>
-                    <p class="title">
+
+                    <p className="title">
                         FULL-STACK SOFTWARE ENGINEER
-                    </p></strong>
+                    </p>
                 </div>
-                
+
+                <div className="link-container">
+                    <Link to='/contact' className="get-in-touch"> GET IN TOUCH &nbsp;&nbsp; 🡥</Link>
+                </div>
+                    
+                <div className="link-container resume">
+                    <a href="/Ben Leland - Resume.pdf" download> DOWNLOAD RESUME &nbsp;&nbsp; {'\u2B73'}</a>
+                </div>
+
             </div>
 
-            <Section title="ABOUT" subtitle="Full-Stack Software Engineer" body={aboutText}/>
+            {/* <Section title="ABOUT" subtitle="Full-Stack Software Engineer" body={aboutText}/> */}
         </div>
     )
 }
