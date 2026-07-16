0979b3a10d6dc803ae81fb05ccd3c2e18ab8c8c8
Ben Leland <ben_leland@comcast.net>
2026-07-15 22:02:16 -0700

    Add final laptop functionality, fix contact page links to open new tab

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -3,14 +3,14 @@ import "./Education.css";
 
 export default function Education({ degree, university, location, progress, date }) {
     return (
-        <div className="education">
+        <div className="section-2">
             <div className="subtitle-group">
                 <p className="symbol">&lt;&gt;</p>
                 <p className="education-degree">{degree}</p>
             </div>
             
-            <p className="education-university">{university} - {location}</p>
-            <p className="education-progress"><strong>{progress}</strong>: {date}</p>
+            <p className="education-info">{university} - {location}</p>
+            <p className="education-info"><strong>{progress}</strong>: {date}</p>
         </div>
     )
 }
